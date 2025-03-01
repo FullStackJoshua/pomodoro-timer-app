ede0e1ea6dedac0a4ebfc23f47f1859d4a4da012
FullStackJoshua <Joshualim5131@gmail.com>
2025-02-28 23:24:08 -0500

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to the Coffee Shop</Text>
+    </View>
+  );
+}
